333c07b5f8b173fc54c2e95c80e180e18b5b04e4
Ruben Dario Gutierrez Sanchez <ruben.gutierrez@adrenalinaestudio.net>
2017-01-30 20:27:34 -0400

    first commit

--- main-componentes-instagram.js
+++ main-componentes-instagram.js
@@ -1,7 +1,7 @@
 
 Vue.component('mis-instantaneas', {
   //props: ['users'], OJO NO SE USA PROPS PARA LA VARIABLE YA QUE ESTA EN DATA
-  template: '<div class="row"><div class="col-md-12" ><ul><liv-for="imagen in instantaneas">{{ imagen.media.note }}</li></ul></div></div>',
+  template: '<div class="row"><div class="col-md-12" ><ul><li v-for="imagen in instantaneas">{{ imagen.media.note }}</li></ul></div></div>',
   mounted(){
       this.cargarImagenes();
   },
