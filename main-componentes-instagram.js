04b1a80e0c4e2734a40be5e5e9801281061e14fe
Ruben Dario Gutierrez Sanchez <ruben.gutierrez@adrenalinaestudio.net>
2017-01-30 20:46:25 -0400

    first commit

--- main-componentes-instagram.js
+++ main-componentes-instagram.js
@@ -1,7 +1,7 @@
 
 Vue.component('mis-instantaneas', {
   //props: ['users'], OJO NO SE USA PROPS PARA LA VARIABLE YA QUE ESTA EN DATA
-  template: '<div class="row"><h1>Pinterest</h1></div>',
+  template: '<div class="row"><ul><li v-for="imagen in instantaneas">{{ imagen.note }}</li></ul></div>',
   mounted(){
       this.cargarImagenes();
   },
@@ -15,7 +15,7 @@ Vue.component('mis-instantaneas', {
       axios.get('https://api.pinterest.com/v1/boards/Tarkiopaulino/robots/pins/?access_token=ATjTZWhnItoRsS4EaYNKQ_yiM-luFJ6yI5O1FnpDv8ou6kApeAAAAAA&fields=id%2Clink%2Cnote%2Curl%2Cimage%2Cmedia')
       .then((respuesta) => {
         console.log(respuesta);
-        this.instantaneas = respuesta.data.data;
+        this.instantaneas = respuesta;
         console.log(this.instantaneas);
         });
     },
