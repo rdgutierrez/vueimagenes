e2ae2f9e306576fbba9c43960339962f69da6f14
Ruben Dario Gutierrez Sanchez <ruben.gutierrez@adrenalinaestudio.net>
2017-01-30 20:33:43 -0400

    first commit

--- main-componentes-instagram.js
+++ main-componentes-instagram.js
@@ -16,7 +16,7 @@ Vue.component('mis-instantaneas', {
       axios.get('https://api.pinterest.com/v1/boards/Tarkiopaulino/robots/pins/?access_token=ATjTZWhnItoRsS4EaYNKQ_yiM-luFJ6yI5O1FnpDv8ou6kApeAAAAAA&fields=id%2Clink%2Cnote%2Curl%2Cimage%2Cmedia')
       .then((respuesta) => {
         console.log(respuesta);
-        this.instantaneas = respuesta.data.data;
+        this.instantaneas = respuesta.data.;
         console.log(this.instantaneas);
         });
     },
